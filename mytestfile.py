e6b8321a1aae27ee5e7087dccec7672b0bd9f540
mbanjestfc <142315833+mbanjestfc@users.noreply.github.com>
2023-08-24 12:27:33 +0100

    yet ano

---
+++ mytestfile.py
@@ -0,0 +1,4 @@
+# The comment `# create a script to check folder size and print that to screen` is providing a
+# description or explanation of what the code is intended to do. It suggests that the script will be
+# used to calculate the size of a folder and display that information on the screen.
+# create a script to check folder size and print that to screen
